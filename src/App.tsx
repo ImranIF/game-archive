b5a6bd887cd0421730a3cfe38898169c4cd525ee
ImranIF <imranfarid1980@outlook.com>
2025-11-09 02:18:14 +0600

    Built a NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import {
+  Button,
+  ButtonGroup,
+  Grid,
+  GridItem,
+  HStack,
+  Show,
+  useBreakpoint,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 const App = () => {
-  return <Button colorPalette={"teal"}>Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"}>
+        <NavBar />
+      </GridItem>
+      {/* when the viewport is lg or larger then Show the aside section */}
+      <Show when={useBreakpointValue({ base: false, lg: true })}>
+        <GridItem area={"aside"} bg={"gold"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"dodgerblue"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
