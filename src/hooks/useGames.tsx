cbcfe80cc640df15418602d8ca336ec44e2d8027
ImranIF <imranfarid1980@outlook.com>
2025-12-01 01:16:34 +0600

    show loading skeletons

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -24,22 +24,28 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController(); // Create an AbortController to manage request cancellation
 
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((response) => setGames(response.data.results))
+      .then((response) => {
+        setGames(response.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return; // Ignore cancellation errors
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort(); // Cleanup function to abort the request on unmount
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
